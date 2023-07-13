286d0c028c06ce6bd30e9f1113483a4e77d29f14
Hatice Akkuş <hticeakks1@gmail.com>
2023-07-13 19:17:06 +0300

    home page created
    added files to be used throughout the project

---
+++ dermaliz/lib/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:dermaliz/login_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/background.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Transform.translate(
+              offset: Offset(-75, 90),
+              child: Text(
+                "Dermaliz",
+                style: TextStyle(
+                  fontFamily: "Nunito",
+                  fontSize: 50,
+                  color: Color(0xFF2F80ED),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        ),
+
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          },
+
+          backgroundColor: Colors.white,
+          child: Icon(
+            Icons.login_outlined,
+            color: Color(0xFF2F80ED),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      ),
+    );
+  }
+}
---
+++ dermaliz/lib/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/background.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
